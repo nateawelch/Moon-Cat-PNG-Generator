569bd825183b12a038f51f9b115444748d60e0ad
Nate Welch <nathanielawelch@gmail.com>
2021-03-12 21:32:28 -0500

    Added collage generation. Committed genesisCats, regularCats, and allCats files with ids.

--- src/index.js
+++ src/index.js
@@ -3,17 +3,63 @@
 
 const yargs = require('yargs')
 const CatTools = require('./CatTools.js')
+const fs = require('fs')
 
 yargs
-    .scriptName("mooncatpnggen")
-    .usage('$0 <cmd> [args]')
-    .command('png [catId]', 'Generate png of a catId', (yargs) => {
-        yargs.positional('catId', {
-            type: 'string',
-            describe: 'catId to generate image for'
-        })
-    }, async function (argv) {
-        CatTools.generateImage(argv.catId)
+  .scriptName("mooncatpnggen")
+  .usage('$0 <cmd> [args]')
+  .command('png [catIds]', 'Generate png of a catId', (yargs) => {
+    yargs.positional('catId', {
+      type: 'string',
+      describe: 'catId to generate image for'
     })
-    .help()
-    .argv
+  }, async function (argv) {
+    CatTools.generateImage(argv.catId)
+    console.log(argv+".png" + " generated")
+
+  })
+  .command('bulkpng [file]', 'Generate pngs from a file containing catIds', (yargs) => {
+    yargs.positional('file', {
+      type: 'string',
+      describe: 'File with list of catIds to generate pngs for'
+    })
+  }, async function (argv) {
+    fs.readFile(argv.file, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      let ids = data.split('\n')
+      if (ids[ids.length - 1] === undefined) ids.pop()
+      process(ids)
+
+    })
+  })
+  .command('collage [file]', "Generate collage of moon cats from a file containing catIds", (yargs) => {
+    yargs.positional('file', {
+      type: 'string',
+      describe: 'File with list of catIds to generate collage for'
+    })
+  }, async function (argv) {
+    fs.readFile(argv.file, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      let ids = data.split('\n')
+      if (ids[ids.length - 1] === undefined) ids.pop()
+      CatTools.generateCollage(ids)
+      console.log("collage.png generated")
+    })
+  })
+  .help()
+  .argv
+
+
+
+function process(ids) {
+  let id = ids.shift()
+  console.log(argv+".png generated")
+  CatTools.generateImage(id)
+  setTimeout(() => { process(ids) }, 5)
+}
